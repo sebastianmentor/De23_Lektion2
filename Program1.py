1c35eb2050121d68e7cc58576944209b45aabac9
Sebastianmentor <sebastian.ohman@systementor.se>
2023-08-31 10:37:50 +0200

    first commit

---
+++ Program1.py
@@ -0,0 +1,20 @@
+print(f"{type(5)=}")
+print(f"{type(5.7)=}")
+print(f"{type('Sträng')=}")
+print(f"{type(None)=}")
+print(f"{type(True)=}")
+print(f"{type(False)=}")
+print(f"{type('S')=}")
+
+print("Dokumentera \nvad vårat \nprogram gör!")
+
+var = 42
+fstring = f'jag vill ha ny rad\n{var + 10}'
+print(fstring)
+fstring2 = f"{var**2}"
+print(type(fstring2)) 
+var2 = "{}".format(var)
+print(var2)
+
+# print(type)
+# print(isinstance(1, float))
